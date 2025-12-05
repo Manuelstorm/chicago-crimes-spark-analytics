525e7ea08abdd04dfbb8413b227318fc5d131aee
Silvio Emanuele Liparoti <102355090+Manuelstorm@users.noreply.github.com>
2025-12-05 19:29:18 +0100

    Add files via upload

---
+++ backend/test/CriminaliSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimes.criminalispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminaliSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
